9595d92504602356ce49f03e89449a2d7fe73c6e
vasiaw <fvlog36@gmail.com>
2026-03-15 12:06:05 +0200

    Homework_c#

---
+++ HOMEWORK_OOP_C#/HOMEWORK_OOP_C#/Program.cs
@@ -0,0 +1,18 @@
+﻿using Class_char_matrix;
+char[,] char_matrix =
+{
+    {'b','c','9'},
+    {'3','2','z'},
+    {'0','4','f'}
+};
+
+CLASS_CHAR_MATRIX matrix = new CLASS_CHAR_MATRIX(char_matrix);
+
+
+Console.WriteLine("column 0: " + matrix[0]);
+Console.WriteLine("column 1: " + matrix[1]);
+Console.WriteLine("column 2: " + matrix[2]);
+
+Console.WriteLine();
+
+Console.WriteLine("The number of digits in the array " + matrix.Digit_Count);
